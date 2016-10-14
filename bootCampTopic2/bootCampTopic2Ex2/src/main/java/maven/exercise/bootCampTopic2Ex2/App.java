5e7f357abd14027387c6df8ac058a793587f3b97
DiegoJStroppiana <diego.stroppiana@globant.com>
2016-10-14 12:49:59 -0300

    Topic 2 - Excercise 2

---
+++ bootCampTopic2/bootCampTopic2Ex2/src/main/java/maven/exercise/bootCampTopic2Ex2/App.java
@@ -0,0 +1,13 @@
+package maven.exercise.bootCampTopic2Ex2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
