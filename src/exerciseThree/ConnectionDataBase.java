e2bef10d1f386c36bc51a062fc9b25c8d033d40f
DiegoJStroppiana <diego.stroppiana@globant.com>
2016-10-26 10:35:10 -0300

    Topic 5 - exercises 1, 2 and 3

--- src/exerciseThree/ConnectionDataBase.java
+++
@@ -1,9 +0,0 @@
-package exerciseThree;
-
-import java.util.Date;
-
-public abstract class ConnectionDataBase {
-	public void sayConnection() {
-		System.out.println(this.getClass().getSimpleName() + " conected to database at: " + new Date());
-		}
-}
--- src/exerciseTwo/AbstractFactory.java
+++
@@ -1,7 +0,0 @@
-package exerciseTwo;
-
-public abstract class AbstractFactory {
-        abstract RelationalDataBase getRelationalDB(String relatinalType);
-        abstract TransactionalDataBase getTransactionalDB(String transactionalType);
-        
-}
